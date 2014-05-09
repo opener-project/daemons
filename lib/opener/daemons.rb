0904db12704e2aae6d29b8e8e52b8ad5a18c2461
Wilco van Duinkerken <wilco@sparkboxx.com>
2014-05-09 09:32:38 +0200

    Add dotenv

--- lib/opener/daemons.rb
+++ lib/opener/daemons.rb
@@ -4,6 +4,10 @@ require "opener/daemons/daemon"
 require "opener/daemons/opt_parser"
 require "opener/daemons/controller"
 
+require "dotenv"
+env_file = File.expand_path("~/.opener-daemons-env")
+Dotenv.load(env_file)
+
 module Opener
   module Daemons
   end
--- opener-daemons.gemspec
+++ opener-daemons.gemspec
@@ -21,6 +21,7 @@ Gem::Specification.new do |spec|
 
   spec.add_dependency 'aws-sdk-core'
   spec.add_dependency 'spoon'
+  spec.add_dependency 'dotenv'
 
   spec.add_development_dependency "bundler", "~> 1.5"
   spec.add_development_dependency "rake"
